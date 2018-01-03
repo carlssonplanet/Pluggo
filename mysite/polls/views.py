78ec16d0d9fc23dd20e3ee3bb0c276b4b731fcf2
Magnus Carlsson <magnus.carlsson@acando.com>
2018-01-03 19:29:46 +0100

    4 done

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -36,4 +36,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
